52318eb71acd280e4b3e6864189851409f8469a6
Diopka <uwagwu.joseph@gmail.com>
2021-01-23 10:11:07 +0100

    User authentication functionality added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
 import expressAsyncHandler from "express-async-handler";
 import Product from "../models/productModel.js";
 import User from "../models/userModel.js";
-import generateToken from '../utils/generateToken.js'
+import generateToken from "../utils/generateToken.js";
 
 // @desc    Auth user & get token
 // @route   POST /api/users/login
@@ -17,11 +17,11 @@ const authUser = expressAsyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: generateToken(user._id)
+      token: generateToken(user._id),
     });
   } else {
     res.status(401);
-    throw new Error('Invalid email or password');
+    throw new Error("Invalid email or password");
   }
 });
 
@@ -48,11 +48,57 @@ const getUserById = expressAsyncHandler(async (req, res) => {
   }
 });
 
+// @desc   Register a new user
+// @route  POST /api/users
+// @access Public
+
+const registerUser = expressAsyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExits = await User.findOne({ email });
+
+  if (userExits) {
+    res.status(400);
+    throw new Error("user already exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("User not found");
+  }
+});
+
 // @desc   Get user profile
 // @route  POST /api/users/profile
 // @access Private
 const getUserProfile = expressAsyncHandler(async (req, res) => {
-  res.send();
-})
+  const user = await User.findById(req.user._id);
 
-export { authUser, getUsers, getUserById, getUserProfile };
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+export { authUser, getUsers, getUserById, registerUser, getUserProfile };
